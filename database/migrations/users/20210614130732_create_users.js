35139d2c9fbf0e419a8d3faa3fa0a69fab46a5ca
esfox <osairis100@gmail.com>
2021-06-14 21:14:59 +0800

    🗄️ Add Users creation migration

---
+++ database/migrations/users/20210614130732_create_users.js
@@ -0,0 +1,26 @@
+const tableName = 'users';
+
+/** @param {import('knex').Knex} knex */
+exports.up = async function(knex)
+{
+  if(await knex.schema.hasTable(tableName))
+    return;
+
+  await knex.schema.createTable(tableName, table =>
+  {
+    table.increments('id');
+
+    table.uuid('uuid').unique().notNullable();
+    table.string('discord_id').index();
+
+    table.date('created_at').index().defaultTo(knex.fn.now());
+    table.date('updated_at').index();
+    table.date('deleted_at').index();
+  });
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function(knex)
+{
+  await knex.schema.dropTableIfExists(tableName);
+};
