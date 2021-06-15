b775ae5152bc7e9a23342fd808ba689586ba0cc8
esfox <osairis100@gmail.com>
2021-06-15 12:07:47 +0800

    🗄️ Create economy migrations

---
+++ database/migrations/discord/20210615024416_create_discord_guilds.js
@@ -0,0 +1,27 @@
+const tableName = 'discord_guilds';
+
+/** @param {import('knex').Knex} knex */
+exports.up = async function(knex)
+{
+  if(await knex.schema.hasTable(tableName))
+    return;
+
+  await knex.schema.createTable(tableName, table =>
+  {
+    table.increments('id');
+
+    table.uuid('discord_id').unique().notNullable();
+    table.string('credits_name').index();
+    table.string('collectibles_name').index();
+
+    table.date('created_at').index().defaultTo(knex.fn.now());
+    table.date('updated_at').index();
+    table.date('deleted_at').index();
+  });
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function(knex)
+{
+  await knex.schema.dropTableIfExists(tableName);
+};
---
+++ database/migrations/economy/20210615004938_create_obtainables.js
@@ -0,0 +1,28 @@
+const tableName = 'obtainables';
+
+/** @param {import('knex').Knex} knex */
+exports.up = async function(knex)
+{
+  if(await knex.schema.hasTable(tableName))
+    return;
+
+  await knex.schema.createTable(tableName, table =>
+  {
+    table.increments('id');
+
+    table.uuid('user_uuid').unique().notNullable();
+    table.string('discord_guild_id').index();
+    table.decimal('amount').defaultTo(0);
+    table.boolean('is_collectible');
+
+    table.date('created_at').index().defaultTo(knex.fn.now());
+    table.date('updated_at').index();
+    table.date('deleted_at').index();
+  });
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function(knex)
+{
+  await knex.schema.dropTableIfExists(tableName);
+};
