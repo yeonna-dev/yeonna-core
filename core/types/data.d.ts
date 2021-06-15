034602401388187b3346734a6332b6bc627b8fbc
esfox <osairis100@gmail.com>
2021-06-15 12:23:35 +0800

    ⚙️ Update type definitions

--- core/types/data.d.ts
+++ core/types/data.d.ts
@@ -1,5 +1,32 @@
+interface User
+{
+  uuid: string;
+  discordID: string;
+}
+
+interface DiscordGuild
+{
+  discordID: string;
+  creditsName: string;
+  collectiblesName: string;
+}
+
 interface Bit
 {
   uuid: string;
   content: string;
+}
+
+interface UserBit
+{
+  userUUID: string;
+  bitUUID: string;
+}
+
+interface Obtainable
+{
+  userUUID: string;
+  discordGuildID: string;
+  amount: number;
+  isCollectible: boolean;
 }
