92b7c7db0d3ebe5057504c6aaa7a5c318008d703
esfox <osairis100@gmail.com>
2021-06-17 23:09:05 +0800

    🗄️ Update obtainable amount size

--- database/migrations/users/20210615004938_create_obtainables.js
+++ database/migrations/users/20210615004938_create_obtainables.js
@@ -12,7 +12,7 @@ exports.up = async function(knex)
 
     table.uuid('user_uuid').unique().notNullable();
     table.string('discord_guild_id').index();
-    table.decimal('amount').defaultTo(0);
+    table.decimal('amount', 20).defaultTo(0);
     table.boolean('is_collectible');
 
     table.date('created_at').index().defaultTo(knex.fn.now());
