e173182064570571a480f2b8625ea4089f4376ea
esfox <osairis100@gmail.com>
2021-06-30 17:56:31 +0800

    ⚙️ Merge type declaration files

---
+++ core/types/index.d.ts
@@ -0,0 +1,63 @@
+/* Data */
+
+interface User
+{
+  uuid: string;
+  discordID?: string | null;
+  twitchID?: string | null;
+}
+
+interface DiscordGuild
+{
+  discordID: string;
+  pointsName: string;
+  collectiblesName: string;
+}
+
+interface Bit
+{
+  uuid: string;
+  content: string;
+}
+
+interface UserBit
+{
+  userUUID: string;
+  bitUUID: string;
+}
+
+interface Obtainable
+{
+  userUUID: string;
+  discordGuildID: string;
+  amount: number;
+  isCollectible?: boolean;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/* Database */
+
+interface TimestampedRecord
+{
+  created_at?: string;
+  updated_at?: string | null;
+  deleted_at?: string | null;
+}
+
+interface UserRecord extends TimestampedRecord
+{
+  uuid: string;
+  discord_id: string | null;
+  twitch_id: string | null;
+}
+
+interface ObtainableRecord extends TimestampedRecord
+{
+  user_uuid: string;
+  amount: number;
+  is_collectible?: boolean;
+  discord_guild_id?: string;
+  twitch_channel_id?: string;
+}
+
