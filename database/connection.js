bd2296bc489222a272d78c53aab653e6881a52da
esfox <osairis100@gmail.com>
2021-09-02 21:41:14 +0800

    🗄️ Separate database migrations to own repository

--- database/connection.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: '.env.db' });
-
-import knex from 'knex';
-
-export default knex({
-  client: process.env.DB_CLIENT,
-  connection: process.env.DB_CONNECTION,
-  migrations: { stub: 'migrations/template.js' }
-});
