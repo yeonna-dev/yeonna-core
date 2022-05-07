540c9bb5afeb903b8b401f786d353e249c68cc65
esfox <osairis100@gmail.com>
2022-05-07 15:13:55 +0800

    Fix transfer obtainables bug

--- src/modules/obtainables/actions/transferObtainables.ts
+++ src/modules/obtainables/actions/transferObtainables.ts
@@ -66,6 +66,7 @@ export const transferObtainables = async ({
         userId: target,
         amount,
         isCollectible,
+        context,
       });
     else
       await ObtainableService.update({
